44dc386b8a05a124b7e893098106e7857db8e817
Shubhu2001 <112776177+Shubhu2001@users.noreply.github.com>
2024-10-09 16:39:03 +0530

    Add files via upload

---
+++ Transaction.js
@@ -0,0 +1,20 @@
+// models/Transaction.js
+
+const mongoose = require('mongoose');
+
+const TransactionSchema = new mongoose.Schema({
+  amount: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Transaction', TransactionSchema);
